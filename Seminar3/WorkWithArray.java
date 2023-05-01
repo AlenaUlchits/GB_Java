e279b273caf50e18c6acf7ecaee5d86b24172fad
AlenaUlchits <alena.ulchic1996@gmail.com>
2023-05-01 10:28:46 +0300

    Seminar3 refactoring

--- Seminar3/WorkWithArray.java
+++ Seminar3/WorkWithArray.java
@@ -22,10 +22,14 @@ public class WorkWithArray {
     }
 
     static void task1(List<Integer> numbers){
+        List<Integer> numbersToRemove = new ArrayList<>();
         for(Integer num : numbers){
             if(num % 2 == 0){
-                numbers.remove(num);
+                numbersToRemove.add(num);
             }
+        }
+        for(Integer num : numbersToRemove){
+            numbers.remove(num);
         }
         printArray(numbers);
     }
@@ -50,7 +54,7 @@ public class WorkWithArray {
                 min = num;
             }*/
         }
-        float average = sum/numbers.size();
+        float average = (float)sum/numbers.size();
 
         System.out.println("Max elem = " + max + ", min elem = " + min + ", average elem = " + average);
     }
@@ -59,7 +63,25 @@ public class WorkWithArray {
         String[] planets = {"Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune",  "Jupiter", "Saturn",
                 "Venus", "Earth","Venus", "Earth"};
         List<String> planetsList = Arrays.asList(planets);
+        Map<String, Integer> planetMap = new HashMap<>();
+        String planet;
+        int count;
+        for(String pl : planetsList){
+            planet = pl;
+            count = 0;
+            for(String p : planetsList) {
+                if (planet.equals(p)) {
+                    count++;
+                }
+            }
+            planetMap.put(planet, count);
+        }
+        printMap(planetMap);
 
-
+    }
+    static void printMap(Map<String, Integer> planets){
+        StringBuilder sb = new StringBuilder();
+        planets.forEach((key, value)-> sb.append(key + " -> " + value + "\n"));
+        System.out.println(sb.toString());
     }
 }
